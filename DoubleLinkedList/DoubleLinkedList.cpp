e050a63145421276eff520601067996fa07bc129
Zharfan0 <zharfanwidyadana@gmail.com>
2023-05-26 16:15:27 +0700

    inserting a Node between two Nodes in the list

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -35,4 +35,33 @@ void DoubleLinkedList::addNode() {
 	Node* newNode = new Node(); //step 1
 	newNode->noMhs = nim; //step 2
 	newNode->name = nm; //step 2
+
+	/*insert a node in the begining of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) { //check if data null
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number non allowed" << endl;
+			return;
+		}
+		newNode->next = START; //step 3
+		if (START != NULL) {
+			START->prev = newNode; //step 4
+			newNode->prev = NULL; // step 5
+			START = newNode; //step 6
+			return;
+		}
+	}
+
+	/*inserting a Node between two Nodes in the list*/
+	Node* current = START; //step 1.a
+	Node* previous = NULL; //step 1.b
+	
+	while (current->next != NULL && current->next->noMhs < nim); { //step 1.c 
+		previous = current; //step 1.d
+		current = current->next; //step 1.e
+	} 
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nduplicate roll numbers non allowerd" << endl;
+	}
+	
 }
